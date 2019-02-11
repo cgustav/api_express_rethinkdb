b7f2c8050f60b6d497c627ca16eba191f23fd1d7
cgustav <gstv.qnd@outlook.com>
2019-02-11 13:35:44 -0300

    fix typo

--- config/models.js
+++ config/models.js
@@ -6,16 +6,16 @@ module.exports = {
     init: async () => {
         const models = require('../api/models/index')
 
-        let data = await find_or_create(models.user, {
+        let user = await find_or_create(models.user, {
             username: 'dropex',
             name: 'Pedro',
-            email: 'pedring@cool.cl',
+            email: 'pedro@pedro.cl',
             auth: {
                 password: '1234'
             }
         })
 
-        let data1 = await find_or_create(models.user, {
+        let user1 = await find_or_create(models.user, {
             username: 'cgustav',
             name: 'Eduardo',
             email: 'edo@edo.cl',
